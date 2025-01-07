1cec7e5782dd0b9b4e5634f10a6a731ca93180cd
Nurunnahar <nurunnahar9081@gmail.com>
2025-01-07 22:18:26 +0600

    testimonial section done

--- exdocs-project/assets/js/main.js
+++ exdocs-project/assets/js/main.js
@@ -1,3 +1,21 @@
-(function ($){
+(function ($) {
     "use strict";
-}) (jquery);
+
+
+
+    var swiper = new Swiper(".tp-textmonial-active", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        keyboard: {
+            enabled: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".tp-swiper-testi-btn-next",
+            prevEl: ".tp-swiper-testi-btn-prev",
+        },
+    });
+})(jQuery);
