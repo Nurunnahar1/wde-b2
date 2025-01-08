d8f81ffda7cc514d8775ceadebbb415224e464ef
Nurunnahar <nurunnahar9081@gmail.com>
2025-01-08 18:40:24 +0600

    brand with slider done

--- exdocs-project/assets/js/main.js
+++ exdocs-project/assets/js/main.js
@@ -1,8 +1,6 @@
 (function ($) {
     "use strict";
-
-
-
+    // for testimonial
     var swiper = new Swiper(".tp-textmonial-active", {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -18,4 +16,23 @@
             prevEl: ".tp-swiper-testi-btn-prev",
         },
     });
+
+    // for brands
+    var swiper = new Swiper(".tp-brand-top-active", {
+        slidesPerView: "auto",
+        spaceBetween: 100,
+        freemode: true,
+        centeredSlides: true,
+        loop: true,
+        allowTouchMove: false,
+        speed: 2000,
+        autoplay: {
+            delay: 1,
+            disableOnInteraction: true,
+        },
+     
+       
+    });
 })(jQuery);
+
+
