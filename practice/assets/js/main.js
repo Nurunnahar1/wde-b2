b9947a92a46930e2d7a5abe9cd009c2c2d2323ca
Nurunnahar <nurunnahar9081@gmail.com>
2025-01-09 21:26:53 +0600

    team section done

--- practice/assets/js/main.js
+++ practice/assets/js/main.js
@@ -1,6 +1,6 @@
 (function ($) {
     "use strict";
-    //  testimonial slider
+    // for testimonial
     var swiper = new Swiper(".tp-textmonial-active", {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -17,7 +17,7 @@
         },
     });
 
-    // brands slider
+    // for brands
     var swiper = new Swiper(".tp-brand-top-active", {
         slidesPerView: "auto",
         spaceBetween: 100,
@@ -30,25 +30,8 @@
             delay: 1,
             disableOnInteraction: true,
         },
-
-
-    });
-
-    //  team slider
-    var swiper = new Swiper(".tp-team-active", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        keyboard: {
-            enabled: true,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".tp-swiper-team-btn-next",
-            prevEl: ".tp-swiper-team-btn-prev",
-        },
+     
+       
     });
 })(jQuery);
 
