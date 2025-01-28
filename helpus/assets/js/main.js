7161d8cf798e9ebc07218a82078baa1792ecffb9
Nurunnahar <nurunnahar9081@gmail.com>
2025-01-28 23:02:29 +0600

    about section

--- helpus/assets/js/main.js
+++ helpus/assets/js/main.js
@@ -1,4 +1,24 @@
 $(function () {
+
+    "use strict";
+
+
+    // data bg img
+    $("[data-bg-img]").each(function () {
+        $(this).css("background-image", "url(" + $(this).attr("data-bg-img") + ")");
+    })
+    // data bg color
+    $("[data-bg-color]").each(function () {
+        $(this).css("background-color", $(this).attr("data-bg-color"));
+    })
+    // data border color
+    $("[data-border-color]").each(function () {
+        $(this).css("border-color", $(this).attr("data-border-color"));
+    })
+    // data color
+    $("[data-color]").each(function () {
+        $(this).css("color", $(this).attr("data-color"));
+    })
     $('.popup-image').magnificPopup({
         type: 'image'
         // other options
